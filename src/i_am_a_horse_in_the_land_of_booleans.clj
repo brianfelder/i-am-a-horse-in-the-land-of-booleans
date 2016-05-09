061960244a932d4f6c0dd42dff33975e4f1ac56a
Felder, Brian <bfelder@cvent.com>
2016-05-08 20:39:43 -0700

    fizzbuzz and generic-doublificate

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -11,7 +11,11 @@
   (if (== (mod n divisor) 0) true false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 15 n) "gotcha!"
+    (divides? 3 n) "fizz"
+    (divides? 5 n) "buzz"
+    :else ""))
 
 (defn teen? [age]
   (< 12 age 20))
@@ -20,7 +24,12 @@
   ":(")
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* x 2)
+    (empty? x) nil
+    (list? x) (* (count x) 2)
+    (vector? x) (* (count x) 2)
+    :else true))
 
 (defn leap-year? [year]
   ":(")
